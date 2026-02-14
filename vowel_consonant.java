765163fccc15cfa72ab2066a49166ab949ce253a
Kaveri123319 <daddykaavi@gmail.com>
2026-02-14 12:28:37 +0530

    Add files via upload

---
+++ vowel_consonant.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class vowel_consonant {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Character:");
+        char ch=sc.next().charAt(0);
+        if(ch=='a'){
+            System.out.println("Vowel");
+        }
+        else if(ch=='e'){
+            System.out.println("Vowel");
+        }
+        else if(ch=='i'){
+            System.out.println("Vowel");
+        }
+        else if(ch=='o'){
+            System.out.println("Vowel");
+        }
+        else if(ch=='u'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+        sc.close();
+    }
+}
