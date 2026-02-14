9872b3481d59e97cb6af7d51a91d85d3c74d7c43
Kaveri123319 <daddykaavi@gmail.com>
2026-02-14 12:29:09 +0530

    Add files via upload

---
+++ print_1to100_num.java
@@ -0,0 +1,10 @@
+public class print_1to100_num {
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=100;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
