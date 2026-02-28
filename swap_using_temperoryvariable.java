525da0715e6d74eb235bfe5d9b76992752267616
Kaveri123319 <daddykaavi@gmail.com>
2026-02-28 09:30:33 +0530

    Add files via upload

---
+++ swap_using_temperoryvariable.java
@@ -0,0 +1,12 @@
+public class swap_using_temperoryvariable {
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=3;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
